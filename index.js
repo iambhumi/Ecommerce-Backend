7f2d59c1031febe12d7f3b4aa0abd28232ef6cec
iambhumi <153648429+iambhumi@users.noreply.github.com>
2025-02-02 19:10:20 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
+const session = require("express-session");
+const MongoStore = require("connect-mongo");
+const Router = require('./routes/Router');
+const app = express();
+
+// Middleware
+app.use(bodyParser.json());
+
+//app.use(session({
+    //secret: 'secret-key',
+    //resave:false,
+    //saveUnintialized : false ,
+    //store: MongoStore.create({mongoUrl: 'mongodb://localhost:27017/'})
+//}));
+
+//Connect to DB
+mongoose.connect('mongodb://localhost:27017/')
+    .then(() => console.log('Connected to DB'))
+    .catch(err => console.log(err));
+
+//Routes
+
+
+
+
+//server 
+const PORT = process.env.PORT || 3000;
+app.get("/",(req , res) =>{
+    res.send("This page is Live ");
+});
+const start = async() => {
+    try{
+        app.listen(PORT, () => {
+            console.log (`${PORT} Yes it is connected `);
+        })
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+start();
