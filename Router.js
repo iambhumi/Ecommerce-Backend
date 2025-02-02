60be403f4bb80893b0173cf7b166e255cf3c74d0
iambhumi <153648429+iambhumi@users.noreply.github.com>
2025-02-02 19:14:03 +0530

    Add files via upload

---
+++ Router.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router();
+const Controller = require('../controllers/Controller');
+
+
+//Product Routes
+router.route("/products").get(Controller.getAllProducts);
+router.route("/productsID").get(Controller.getProductByID);
+router.route("/cart").get(Controller.getCart);
+router.route("/cart").post(Controller.addToCart);
+router.route("/cart/:ID").delete(Controller.removeFromCart);
+
+//User Routes
+router.route("/register").post(Controller.RegisterUser);
+router.route("/login").post(Controller.loginUser);
+router.route("/Logout").get(Controller.logoutUser);
+
+module.exports = router;
+
